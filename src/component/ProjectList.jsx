677ffa24348bde7d8eb73a9ddb764eb76e92dbab
Anna-Edel <125572175+Anna-Edel@users.noreply.github.com>
2025-01-07 17:40:10 +0100

    corrections

--- src/component/ProjectList.jsx
+++ src/component/ProjectList.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import PropTypes from 'prop-types';
 
 export default function ProjectList({ projects }) {
     return (
         <div className='projectList'>
-            {projects.map((projects) => {
-                return <div>
-                    <img className='img' src={projects.img} alt={projects.category} />
+            {projects.map((project, index) => (
+                <div key={project.id || index}> {/* Используем id, если есть, иначе индекс */}
+                    <img className='img' src={project.img} alt={project.category} />
                 </div>
-            })}
+            ))}
         </div>
-    )
+    );
 }
+
+ProjectList.propTypes = {
+    projects: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            img: PropTypes.string.isRequired,
+            category: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
--- src/component/Toolbar.jsx
+++ src/component/Toolbar.jsx
@@ -1,22 +1,23 @@
-import uniquid from 'uniquid';
 import PropTypes from 'prop-types';
 
 export default function Toolbar({ filters, selected, onSelectFilter }) {
     return (
         <div className="toolbar">
-            {filters.map((filter) => {
-                return (
-                    <div className={filter === selected ? 'toolbar-btn toolbar-btn-current' : 'toolbar-btn'}
-                        onClick={() => onSelectFilter(filter)} key={uniquid()}>{filter}
-                    </div>
-                );
-            })}
+            {filters.map((filter, index) => (
+                <div
+                    className={filter === selected ? 'toolbar-btn toolbar-btn-current' : 'toolbar-btn'}
+                    onClick={() => onSelectFilter(filter)}
+                    key={index}
+                >
+                    {filter}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 Toolbar.propTypes = {
-    filters: PropTypes.array,
-    selected: PropTypes.string,
-    onSelectFilter: PropTypes.func
-}
+    filters: PropTypes.arrayOf(PropTypes.string).isRequired,
+    selected: PropTypes.string.isRequired,
+    onSelectFilter: PropTypes.func.isRequired,
+};
